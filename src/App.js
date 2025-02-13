09acab9fb05cfe7e25458fb7b219a49e27aef44b
Ermol <00ermzahar@gmail.com>
2025-02-13 23:03:35 +0300

    Initial Create repository for project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 import Catalog from './components/Catalog.js';
 
